174ae3155e577856337464c35e0fa7cb7f52c6c3
Juraj Sranko <jus@sranko-informatik.de>
2020-06-08 12:43:25 +0200

    Anpassungen an der Suchelogik

--- si_show_view_dependencies.sql
+++ si_show_view_dependencies.sql
@@ -1,13 +1,13 @@
 -- UDTF:VIEW_DEPENDENCIES determines view dependencies
 
 
-Create or Replace Function VIEW_DEPENDENCIES(File Varchar(10), 
-                                             Library Varchar(10))
+Create or Replace Function SI_VIEW_DEPENDENCIES(File Varchar(10), 
+                                                Library Varchar(10))
 Returns Table (Level Integer, 
                File Varchar(10),
                Library Varchar(10))
 Language Sql
-Specific VIEWDEPS
+Specific SIVWDEPS
 Not Deterministic
 No External Action
 Returns Null On Null Input
@@ -15,35 +15,32 @@ Set Option Dbgview = *Source
 Begin Atomic
 
 Return 
-With Recursive Dependents (calllevel, dbffil, dbflib, dbffdp, dbfldp) As (
-          Select 1 As calllevel , dbffil , dbflib , dbffdp , dbfldp 
+With base As (
+          Select dbffil , dbflib , dbffdp , dbfldp , dbftdp
             From qadbfdep 
+        Group By dbffil , dbflib , dbffdp , dbfldp , dbftdp
+), Dependents (calllevel, dbffil, dbflib, dbffdp, dbfldp) As (
+          Select 1 As calllevel , dbffil , dbflib , dbffdp , dbfldp 
+            From base 
            Where dbffil = File And dbflib = Library 
            Union All 
           Select calllevel + 1 As calllevel, b.dbffil, b.dbflib, b.dbffdp, b.dbfldp 
             From Dependents a 
-            Join qadbfdep b 
+            Join base b 
               On (a.dbffdp, a.dbfldp) = (b.dbffil, b.dbflib) 
 ), dependencies As (
-          Select calllevel, dbffil, dbflib, dbffdp, dbfldp 
-            From Dependents
-        Group By calllevel, dbffil, dbflib, dbffdp, dbfldp 
-        Order By calllevel, dbffil Asc
-), uni As (
-          Select calllevel, dbffil, dbflib 
-            From dependencies
-           Union    
           Select calllevel, dbffdp, dbfldp 
-            From dependencies a
-           Where Not Exists (Select * From dependencies b Where (b.dbffil, b.dbflib) = (a.dbffdp, a.dbfldp))
+            From Dependents
+        Group By calllevel, dbffdp, dbfldp 
 )
           Select *
-            From uni
-        Order By calllevel, dbffil
+            From dependencies 
+        Order By calllevel, dbffdp Asc
 ;
 End
 ;
+
 -- Run it
                                
-select * from table(VIEW_DEPENDENCIES('SYSVIEWS', 'QSYS2')) as a
+select * from table(SI_VIEW_DEPENDENCIES('SYSVIEWS', 'QSYS2')) as a
 ;
